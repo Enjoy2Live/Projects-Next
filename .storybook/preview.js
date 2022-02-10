792a5a7556ccde3eaf173ea6042557dc6ee17406
Enjoy2Live <iMohammedMaqbol@gmail.com>
2022-02-10 08:54:11 +0300

    inital commit

---
+++ .storybook/preview.js
@@ -0,0 +1,40 @@
+import React from "react";
+import * as NextImage from "next/image";
+import { ThemeProvider } from "styled-components";
+import theme from "../src/styles/theme";
+import GlobalStyle from "../src/styles/globals";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+// Global decorator to apply the styles to all stories
+export const decorators = [
+  (Story) => (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Story />
+      </ThemeProvider>
+    </>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  a11y: {
+    // the target DOM element
+    element: "#root",
+    // sets the execution mode for the addon
+    manual: false,
+  },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
---
+++ src/components/modules/Projects/Project/Description/DescriptionContent/DescriptionContent.js
@@ -0,0 +1,34 @@
+import ReactMarkdown from "react-markdown";
+import {
+  Paragrapgh,
+  FlexSection,
+  ImageHolder,
+  Conatiner,
+  Strip,
+} from "./StyledDescriptionContent";
+
+const DescriptionContent = ({ data }) => {
+  const { description, images } = data;
+  return (
+    <>
+      <Paragrapgh style={{ paddingBottom: "1.5625rem" }}>
+        {" "}
+        {/* eslint-disable-next-line react/no-children-prop */}
+        <ReactMarkdown children={description} />
+      </Paragrapgh>
+      {!!images?.length && (
+        <Strip>
+          <Conatiner>
+            <FlexSection>
+              {images.map((image) => (
+                <ImageHolder key={image.id} src={image.url} />
+              ))}
+            </FlexSection>
+          </Conatiner>
+        </Strip>
+      )}
+    </>
+  );
+};
+
+export default DescriptionContent;
